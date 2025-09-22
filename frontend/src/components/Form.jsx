d1638b6e68a0492bbd1137e4e748636913eb8019
Shyamkrishna B Nair <shyamkrishnabnair@gmail.com>
2025-09-22 21:21:10 +0530

    front end Login/Register

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,6 +2,7 @@ import {use, useState} from 'react'
 import api from "../api"
 import { useNavigate } from 'react-router-dom'
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants'  
+import "../styles/Form.css"
 
 function Form({route, method}) {
     const [username,setUsername]=useState('')
@@ -51,3 +52,5 @@ function Form({route, method}) {
         </button>
     </form>
 }
+
+export default Form
